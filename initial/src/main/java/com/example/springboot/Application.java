75cb2e816660bbf455448b20de33c74696f98000
yui728 <yui.nishimra.1984@gmail.com>
2020-09-15 00:44:40 +0900

    エンドポイントの実行まで実装

--- initial/src/main/java/com/example/springboot/Application.java
+++ initial/src/main/java/com/example/springboot/Application.java
@@ -2,23 +2,40 @@ package com.example.springboot;
 
 import java.util.Arrays;
 
+import com.sun.tools.javac.main.CommandLine;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class, args);
+//		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+//
+//		System.out.println("Let's inspect the beans provided by Spring Boot:");
+//
+//		String[] beanNames = ctx.getBeanDefinitionNames();
+//		Arrays.sort(beanNames);
+//		for (String beanName : beanNames) {
+//			System.out.println(beanName);
+//		}
+	}
 
-		System.out.println("Let's inspect the beans provided by Spring Boot:");
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
 
-		String[] beanNames = ctx.getBeanDefinitionNames();
-		Arrays.sort(beanNames);
-		for (String beanName : beanNames) {
-			System.out.println(beanName);
-		}
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+		};
 	}
 	
 }
